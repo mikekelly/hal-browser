d39bfb75b28bf32a2b9e338369bb99f4ea41838e
Mike Kelly <mikekelly321@gmail.com>
2013-03-17 22:58:39 +0000

    make documentation button work

--- js/hal/views/inspector.js
+++ js/hal/views/inspector.js
@@ -5,9 +5,6 @@ HAL.Views.Inspector = Backbone.View.extend({
     _.bindAll(this, 'renderDocumentation');
     _.bindAll(this, 'renderResponse');
 
-    this.responseView = new HAL.Views.Response({ vent: this.vent });
-    this.docView = new HAL.Views.Documenation({ vent: this.vent });
-
     this.vent.bind('show-docs', this.renderDocumentation);
     this.vent.bind('response', this.renderResponse);
   },
@@ -19,17 +16,21 @@ HAL.Views.Inspector = Backbone.View.extend({
   },
 
   renderResponse: function(response) {
-    this.render();
-    this.responseView.render(response);
+    var responseView = new HAL.Views.Response({ vent: this.vent });
 
-    this.$el.append(this.responseView.el);
+    this.render();
+    responseView.render(response);
+
+    this.$el.append(responseView.el);
   },
 
-  renderDocumentation: function(url) {
-    this.render();
-    this.docView.render(url);
+  renderDocumentation: function(e) {
+    var docView = new HAL.Views.Documenation({ vent: this.vent });
 
-    this.$el.append(this.docView);
+    this.render();
+    docView.render(e.url);
+
+    this.$el.append(docView.el);
   },
 
   template: function() {

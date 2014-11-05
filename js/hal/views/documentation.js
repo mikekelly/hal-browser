251c3e1ee32c1c7aedce292eba3bba30595aa025
bsodzik <bsodzik@tlen.pl>
2014-11-05 10:47:26 +0100

    Force docs (curies) to be downloaded as JSON.

--- js/hal/views/documentation.js
+++ js/hal/views/documentation.js
@@ -2,6 +2,15 @@ HAL.Views.Documenation = Backbone.View.extend({
   className: 'documentation',
 
   render: function(url) {
-    this.$el.html('<iframe src=' + url + '></iframe>');
+    this.$el.html('<pre id="docSection">Loading...</pre>');
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      success: function(resource) {
+        $('#docSection').text(JSON.stringify(resource, null, '  '));
+      }
+    }).error(function() {
+      $('#docSection').text('Error: Unable to get documentation');
+    });
   }
 });

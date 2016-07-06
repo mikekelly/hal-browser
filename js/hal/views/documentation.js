efefdf111b467bad9952c612a2a23fc0ad70fa3f
Jacek Kobus <jacek.kobus@audioteka.pl>
2016-07-06 11:07:46 +0200

    fixed iframe beeing blocked due to insecure requests when using https

--- js/hal/views/documentation.js
+++ js/hal/views/documentation.js
@@ -2,6 +2,6 @@ HAL.Views.Documenation = Backbone.View.extend({
   className: 'documentation',
 
   render: function(url) {
-    this.$el.html('<iframe src=' + url + '></iframe>');
+    this.$el.html('<iframe src="' + url + '"></iframe>');
   }
 });
